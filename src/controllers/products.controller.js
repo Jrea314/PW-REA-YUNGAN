9dfdf4906afc79c263992d1566d92763f7f97c1c
Jrea314 <94095443+Jrea314@users.noreply.github.com>
2022-02-28 22:29:27 -0500

    Codigo para el CRUD y DB
    Crud y conecxion del database

---
+++ src/controllers/products.controller.js
@@ -0,0 +1,19 @@
+export const createProdudct = (req, res) => {
+
+}
+
+export const getProdudcts = (req, res) => {
+    res.json("get products")
+}
+
+export const getProdudctById = (req, res) => {
+    
+}
+
+export const updateProdudctById = (req, res) => {
+    
+}
+
+export const deleteProdudctById = (req, res) => {
+    
+}
