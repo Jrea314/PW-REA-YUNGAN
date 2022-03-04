923065f6f049162e05566821e789ba9188321489
Jrea314 <94095443+Jrea314@users.noreply.github.com>
2022-03-03 20:53:33 -0500

    Update index.js

--- build/index.js
+++ build/index.js
@@ -4,4 +4,4 @@ var _express = _interopRequireDefault(require("express"));
 function _interopRequireDefault(obj) { return obj && obj._esModule ? obj : { "default": obj }; }
 var app = (0, _express["default"])();
 app.listen(4000);
-console.log('Server Listen on port', 4000)
+console.log('El servidor se escucha por el puerto ', 4000)
